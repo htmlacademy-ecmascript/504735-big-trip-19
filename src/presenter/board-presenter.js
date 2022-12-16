d4ee4a66a2b99988d371914314e4ec0265e71c97
AndreyAh <38111898+AndreyAh@users.noreply.github.com>
2022-12-16 14:39:47 +0700

    Выполнил задание шаблонизируй это!

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,28 +1,30 @@
 import BoardView from '../view/board-view.js';
-import SortView from './../view/list-sort';
-import EventListView from './../view/event-list.js';
-import FormOfCreationView from './../view/form-of-creation.js';
-import EditingFormView from './../view/editing-form';
-import RoutPointView from './../view/route-point.js';
+import SortView from './../view/list-sort-view.js';
+import EventListView from '../view/event-list-view.js';
+import EditingFormView from '../view/editing-form-view';
+import RoutPointView from '../view/route-point-view.js';
 import {render} from '../render.js';
 
 export default class BoardPresenter {
   boardComponent = new BoardView();
   eventListComponent = new EventListView();
 
-  constructor({boardContainer}) {
+  constructor({boardContainer, pointsModel}) {
     this.boardContainer = boardContainer;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    //Сохраняем данные полученные из модели.
+    this.boardPoints = [...this.pointsModel.getPoints()];
+    this.destinations = this.pointsModel.getAllDestinations();
+    this.offers = this.pointsModel.getAllOffersByType();
     render(this.boardComponent, this.boardContainer);
     render(new SortView(), this.boardComponent.getElement());
-    render(new FormOfCreationView, this.boardComponent.getElement());
     render(this.eventListComponent, this.boardContainer);
-    render(new EditingFormView(), this.eventListComponent.getElement());
-
-    for (let i = 0; i < 3; i++) {
-      render(new RoutPointView(), this.eventListComponent.getElement());
+    render(new EditingFormView({point: this.boardPoints[0], destinations: this.destinations, offers: this.offers}), this.eventListComponent.getElement());
+    for (let i = 1; i < this.boardPoints.length; i++) {
+      render(new RoutPointView({point: this.boardPoints[i], destinations: this.destinations, offers: this.offers}), this.eventListComponent.getElement());
     }
   }
 }
